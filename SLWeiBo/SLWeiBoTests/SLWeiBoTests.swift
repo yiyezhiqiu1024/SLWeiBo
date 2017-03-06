4665b46dc7bef681fb81489b49e4fffe1a024289
CoderSLZeng <359297567@qq.com>
2017-03-06 22:32:58 +0800

    部署项目

---
+++ SLWeiBo/SLWeiBoTests/SLWeiBoTests.swift
@@ -0,0 +1,36 @@
+//
+//  SLWeiBoTests.swift
+//  SLWeiBoTests
+//
+//  Created by Anthony on 17/3/6.
+//  Copyright © 2017年 SLZeng. All rights reserved.
+//
+
+import XCTest
+@testable import SLWeiBo
+
+class SLWeiBoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
