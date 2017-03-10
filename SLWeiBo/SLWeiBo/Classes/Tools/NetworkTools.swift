edbe750e24bda4376a8a4edcd452d45e73adc168
CoderSLZeng <359297567@qq.com>
2017-03-10 14:59:55 +0800

    创建网络工具类，封装AFNetworking代码

---
+++ SLWeiBo/SLWeiBo/Classes/Tools/NetworkTools.swift
@@ -0,0 +1,68 @@
+//
+//  NetworkTools.swift
+//  SLWeiBo
+//
+//  Created by Anthony on 17/3/10.
+//  Copyright © 2017年 SLZeng. All rights reserved.
+//  AFNetworking的封装
+//
+
+import AFNetworking
+
+// 定义枚举类型
+enum RequestType : String {
+    case GET = "GET"
+    case POST = "POST"
+}
+
+class NetworkTools: AFHTTPSessionManager {
+    // let是线程安全的
+    static let shareInstance : NetworkTools = {
+        let url = NSURL(string: "https://api.weibo.com/")
+        let instance = NetworkTools(baseURL: url, sessionConfiguration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        instance.responseSerializer.acceptableContentTypes?.insert("text/plain")
+        
+        return instance
+    }()
+}
+
+// MARK:- 封装请求方法
+extension NetworkTools {
+    func request(methodType : RequestType, urlString : String, parameters : [String : AnyObject], finished : (result : AnyObject?, error : NSError?) -> ()) {
+        
+        // 1.定义成功的回调闭包
+        let successCallBack = { (task : NSURLSessionDataTask, result : AnyObject?) -> Void in
+            finished(result: result, error: nil)
+        }
+        
+        // 2.定义失败的回调闭包
+        let failureCallBack = { (task : NSURLSessionDataTask?, error : NSError) -> Void in
+            finished(result: nil, error: error)
+        }
+        
+        // 3.发送网络请求
+        if methodType == .GET {
+            GET(urlString, parameters: parameters, progress: nil, success: successCallBack, failure: failureCallBack)
+        } else {
+            POST(urlString, parameters: parameters, progress: nil, success: successCallBack, failure: failureCallBack)
+        }
+        
+    }
+}
+
+
+// MARK:- 请求AccessToken
+extension NetworkTools {
+    func loadAccessToken(code : String, finished : (result : [String : AnyObject]?, error : NSError?) -> ()) {
+        // 1.获取请求的URLString
+        let urlString = "oauth2/access_token"
+        
+        // 2.获取请求的参数
+        let parameters = ["client_id" : WB_App_Key, "client_secret" : WB_App_Secret, "grant_type" : "authorization_code", "redirect_uri" : WB_Redirect_URI, "code" : code]
+        
+        // 3.发送网络请求
+        request(.POST, urlString: urlString, parameters: parameters) { (result, error) -> () in
+            finished(result: result as? [String : AnyObject], error: error)
+        }
+    }
+}
