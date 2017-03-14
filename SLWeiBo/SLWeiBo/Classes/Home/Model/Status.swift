974a833fc582d5dc7e6c292caffdc7b7471c123b
CoderSLZeng <359297567@qq.com>
2017-03-14 10:16:14 +0800

    微博数据处理 1.来源处理 2.时间处理

--- SLWeiBo/SLWeiBo/Classes/Home/Model/Status.swift
+++ SLWeiBo/SLWeiBo/Classes/Home/Model/Status.swift
@@ -12,12 +12,47 @@ class Status: NSObject {
     // MARK:- 属性
     /// 微博创建时间
     var created_at : String?
+    {
+        didSet {
+            // 1.nil值校验
+            guard let created_at = created_at else {
+                return
+            }
+            
+            // 2.对时间处理
+            createAtText = NSDate.createDateString(created_at)
+        }
+
+    }
     /// 微博来源
     var source : String?
+    {
+        didSet {
+            // 1.nil值校验
+            guard let source = source where source != "" else {
+                return
+            }
+            
+            // 2.对来源的字符串进行处理
+            // 2.1.获取起始位置和截取的长度
+            let startIndex = (source as NSString).rangeOfString(">").location + 1
+            let length = (source as NSString).rangeOfString("</").location - startIndex
+            
+            // 2.2.截取字符串
+            sourceText = (source as NSString).substringWithRange(NSRange(location: startIndex, length: length))
+        }
+    }
+    
     /// 微博的正文
     var text : String?
     /// 微博的ID
     var mid : Int = 0
+    
+    // MARK:- 对数据处理的属性
+    /// 处理过的微博来源
+    var sourceText : String?
+    /// 处理过的微博创建时间
+    var createAtText : String?
     
     // MARK:- 自定义构造函数
     init(dict : [String : AnyObject]) {
